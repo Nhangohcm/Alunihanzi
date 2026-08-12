91baf21b4cde5262fe4f63bbe437ff563becd696
Nhangohcm <nhangohcm@gmail.com>
2026-08-12 23:10:55 +0700

    Add files via upload

--- access-config.js
+++ access-config.js
@@ -1,3 +1,1 @@
-window.ALUNI_API_BASE = 'https://aluni-tts.nhangohcm.workers.dev';
-window.ALUNI_FREE_FALLBACK_COURSE = '50plus';
-window.ALUNI_FREE_FALLBACK_LESSONS = 10;
+window.ALUNI_API_BASE = 'https://aluni-v38-test.nhangohcm.workers.dev';
