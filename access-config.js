6e7110410bb01c7ac5665746976aeb48f57e09f5
Nhangohcm <nhangohcm@gmail.com>
2026-08-16 16:32:19 +0700

    Update access-config.js

--- access-config.js
+++ access-config.js
@@ -1,1 +1,2 @@
-window.ALUNI_API_BASE = 'https://aluni-tts.nhangohcm.workers.dev';
+// V51.3: Dùng API cùng tên miền để tương thích trình duyệt điện thoại/Zalo.
+window.ALUNI_API_BASE = window.location.origin;
