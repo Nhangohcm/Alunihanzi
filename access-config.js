ff6b55183459b500887dd9d1c9a9ce776877f509
Nhangohcm <nhangohcm@gmail.com>
2026-08-30 17:26:44 +0700

    Update access-config.js
    Use staging Worker in access config

--- access-config.js
+++ access-config.js
@@ -1,4 +1,4 @@
-window.ALUNI_API_BASE = 'https://aluni-tts.nhangohcm.workers.dev';
+window.ALUNI_API_BASE = 'https://aluni-tts-staging.nhangohcm.workers.dev;
 
 window.ALUNI_SALES_PAGE_URL =
   'https://tieng-trung-aluni.nhangohcm.chatgpt.site/';
