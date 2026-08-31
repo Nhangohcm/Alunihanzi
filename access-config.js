5c873b24ea25cf372e3213c53f8e21b1fc77974e
Nhangohcm <nhangohcm@gmail.com>
2026-08-31 09:33:27 +0700

    Tải mô-đun Kho Viết chữ V2

--- access-config.js
+++ access-config.js
@@ -2,3 +2,6 @@ window.ALUNI_API_BASE = 'https://aluni-tts.nhangohcm.workers.dev';
 
 window.ALUNI_SALES_PAGE_URL =
   'https://tieng-trung-aluni.nhangohcm.chatgpt.site/';
+
+// Giao diện Kho Viết chữ V2 được tách riêng để dễ thử nghiệm và hoàn tác qua PR.
+document.write('<script src="writing-library-v2.js?v=1"><' + '/script>');
